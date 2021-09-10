35f58e85b58a50b8ece15d5dd0124efc3d4c793b
souvik0306 <souvikdatta123@gmail.com>
2021-09-10 22:09:49 +0530

    Read Image and Video

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-import cv2 as cv
-img = cv.imread('Photos/cat.jfif')
-cv.imshow('Cat',img)
+# import cv2 as cv
+# img = cv.imread('Photos/cat.jfif')
+# cv.imshow('Cat',img)
+# cv.waitKey(0)
 
-cv.waitKey(0)
+
+import cv2 as cv
+capture = cv.VideoCapture('Photos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+capture.release()
+capture.destroAllWindows
+
