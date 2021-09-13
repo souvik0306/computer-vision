155ae2b078eeefd2a0a7f10a0dfdc5d5d3426590
souvik0306 <souvikdatta123@gmail.com>
2021-09-13 14:11:23 +0530

    Creative Shapes

--- test.py
+++ test.py
@@ -34,15 +34,33 @@
 # capture.release()
 # capture.destroAllWindows
 
-#Resize Images -
+# Resize Images -
+# import cv2 as cv
+# img = cv.imread('Photos/python.jpg')
+#
+# width,height = 700,500
+# imgnew = cv.resize(img,(width,height))
+#
+# cv.imshow('New',imgnew)
+# print(img.shape)
+# print(imgnew.shape)
+#
+# cv.waitKey(0)
+
 import cv2 as cv
-img = cv.imread('Photos/python.jpg')
+import numpy as np
 
-width,height = 700,500
-imgnew = cv.resize(img,(width,height))
+blank = np.zeros((400, 500, 3), dtype='uint8')
 
-cv.imshow('New',imgnew)
-print(img.shape)
-print(imgnew.shape)
+blank[:] = 0, 0, 0  # B,G,R  format in Python
+# #use Color Picker from Google to generate HEX code for RGB
+
+# this generates a green box in the blue image
+cv.circle(blank, radius=30, center=(250, 200), thickness=2, color=(0, 250, 0))
+cv.line(blank, (250, 200), (250, 172), thickness=2, color=(0, 0, 250))  # upper
+cv.line(blank, (250, 200), (272, 218), thickness=2, color=(0, 0, 250))  # right
+cv.line(blank, (250, 200), (228, 218), thickness=2, color=(0, 0, 250))  # left
+
+cv.imshow('Shapes', blank)
 
 cv.waitKey(0)
