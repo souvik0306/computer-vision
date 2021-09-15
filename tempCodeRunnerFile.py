f1b762b55267f59c1698420a885a99ebab0ee30f
souvik0306 <souvikdatta123@gmail.com>
2021-09-15 21:45:52 +0530

    Sorted

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-capture = cv.VideoCapture(0)
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
-    if cv.waitKey(20) & 0xFF == ord('d'):
-        break
-capture.release()
-capture.destroAllWindows
