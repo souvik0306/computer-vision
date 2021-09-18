7402c92b6fd3f17619b33b3b73bba4fb03b9d0b0
souvik0306 <souvikdatta123@gmail.com>
2021-09-18 17:24:47 +0530

    Face Detect over Video

--- FaceDetection Image.py
+++ FaceDetection Image.py
@@ -9,7 +9,7 @@ new = cv.resize(img, (width, height))
 gray = cv.cvtColor(new, cv.COLOR_BGR2GRAY)
 
 haar_cascade = cv.CascadeClassifier(r'D:/computer-vision/haar_face.xml')
-faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
 # min is the min number of rectangles the code must identify to declare it as a face
 # on setting the scale close to 1.1 and 3 or 4 we are accurately able to get all 18 faces correctly
 # on increasing the min> 3, we are encountering errors of +1 or -1
@@ -19,16 +19,5 @@ cv.imshow('Final',new)
 print(f'Number of faces: {len(faces)}')
 cv.waitKey(0)
 
-# capture = cv.VideoCapture(0)
+capture = cv.VideoCapture(0)
 
-# while True:
-#     isTrue, frame = capture.read()
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-#     haar_cascade = cv.CascadeClassifier('haar_face.xml')
-#     faces = haar_cascade.detectMultiScale(gray,scaleFactor=1.1, miniNeighbors=3)
-#     cv.imshow('Video', gray)
-#     if cv.waitKey(5) & 0xFF == ord('d'):  # d is the kill swich here
-#         break
-
-# capture.release()
-# capture.destroAllWindows
