6700b43a718639534b93b58e30cf153ae342005d
souvik0306 <souvikdatta123@gmail.com>
2021-09-19 16:55:59 +0530

    Face Det. Correction

--- Face Detection Loop.py
+++ Face Detection Loop.py
@@ -12,10 +12,10 @@ people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kalin
 fac_recognizer = cv.face.LBPHFaceRecognizer_create()
 fac_recognizer.read('face_trained.yml')
 
-p = (r'D:\computer-vision\Photos\Faces\val\mindy_kaling')
+# p = (r'D:\computer-vision\Photos\Faces\val\mindy_kaling')
 # p = (r'D:\computer-vision\Photos\Faces\val\elton_john')
 # p = (r'D:\computer-vision\Photos\Faces\val\madonna')
-# p = (r'D:\computer-vision\Photos\Faces\val\ben_affleck')
+p = (r'D:\computer-vision\Photos\Faces\val\ben_afflek')
 
 for filepath in os.listdir(p):
     img = cv.imread(os.path.join(p, filepath))
