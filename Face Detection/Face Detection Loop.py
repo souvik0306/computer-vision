64a5cadcb749d7e0a23d2a8f854fe1340a459e8f
souvik0306 <souvikdatta123@gmail.com>
2021-09-19 17:05:34 +0530

    Sorted

---
+++ Face Detection/Face Detection Loop.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+import os
+import pathlib as Path
+
+haar_cascade = cv.CascadeClassifier(r'D:\computer-vision\Face Detection\haar_face.xml')
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+# features = np.load('features.npy',allow_pickle=True)
+# labels = np.load('labels.npy')
+
+fac_recognizer = cv.face.LBPHFaceRecognizer_create()
+fac_recognizer.read(r'D:\computer-vision\Face Detection\face_trained.yml')
+
+# p = (r'D:\computer-vision\Photos\Faces\val\mindy_kaling')
+# p = (r'D:\computer-vision\Photos\Faces\val\elton_john')
+p = (r'D:\computer-vision\Photos\Faces\val\madonna')
+# p = (r'D:\computer-vision\Photos\Faces\val\ben_afflek')
+
+for filepath in os.listdir(p):
+    img = cv.imread(os.path.join(p, filepath))
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    cv.imshow('Person',gray)
+    
+    fac_recog = haar_cascade.detectMultiScale(gray,scaleFactor=1.1, minNeighbors=4)
+    for (x,y,w,h) in fac_recog:
+        face_roi = gray[y:y+h,x:x+w]
+        labels, confidence = fac_recognizer.predict(face_roi)
+        print(f'Labels = {people[labels]} with a confidence = {int(confidence)} %')
+        cv.waitKey(3)
+
+
+
---
+++ Face Detection/Face Detection over Image.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread(r'D:/computer-vision/Photos/people2.jpg')
+
+width, height = 700, 500
+new = cv.resize(img, (width, height))
+
+gray = cv.cvtColor(new, cv.COLOR_BGR2GRAY)
+
+haar_cascade = cv.CascadeClassifier(r'D:\computer-vision\Face Detection\haar_face.xml')
+faces = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+# min is the min number of rectangles the code must identify to declare it as a face
+# on setting the scale close to 1.1 and 3 or 4 we are accurately able to get all 18 faces correctly
+# on increasing the min> 3, we are encountering errors of +1 or -1
+for (x,y,w,h) in faces:
+    cv.rectangle(new,(x,y),(x+w,y+h),(0,255,0),thickness=2)
+cv.imshow('Final',new)
+print(f'Number of faces: {len(faces)}')
+cv.waitKey(0)
+
+capture = cv.VideoCapture(0)
+
