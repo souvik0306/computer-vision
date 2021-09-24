d3c78e7ea0519b02622ce958d6408f1c6654bfdc
souvik0306 <souvikdatta123@gmail.com>
2021-09-24 08:16:57 +0530

    Sorted

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-
-img = cv.imread(r'D:\computer-vision\Photos\street.jpg')
-width,height = 700,500
-new = cv.resize(img,(width,height))
-hsv = cv.cvtColor(new,cv.COLOR_BGR2HSV)
-cv.imshow('People',hsv)
-cv.waitKey(0)
